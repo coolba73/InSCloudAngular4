9def31bc63c5ed01374ca20fbe065e4381076918
jaehoon choi <jaehoon choi>
2017-05-08 17:11:24 +0900

    1

--- src/app/test/modaltest.component.ts
+++ src/app/test/modaltest.component.ts
@@ -1,8 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ng2-bootstrap/modal/modal.component';
+import { DialogService } from "ng2-bootstrap-modal";
+import { ConfirmComponent } from "../MyComponent/Modal/confirm.component";
 
 @Component({
     templateUrl:'modaltest.component.html'
 })
 export class ModalTestComponent{
 
+    constructor(private dialogService:DialogService){}
+
+    showConfirm()
+    {
+        let disposable = this.dialogService.addDialog(ConfirmComponent,{
+            title:'test',
+            message:'test'
+        }).subscribe((isConfirmed) =>{
+            if (isConfirmed)
+            {
+                alert('accept');
+            }
+            else
+            {
+                alert('declined');
+            }
+        });
+    }
 }//class
--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from "@angular/core";
-
 import { ModalTestComponent } from "./modaltest.component";
-
 import { TestRoutingModule } from "./test.routing";
+import { ModalModule } from 'ng2-bootstrap/modal';
+import { BootstrapModalModule } from "ng2-bootstrap-modal";
+import { ConfirmComponent } from "../MyComponent/Modal/confirm.component";
 
 @NgModule({
-    imports : [TestRoutingModule],
+    imports : [
+        TestRoutingModule
+        ,ModalModule.forRoot()
+        ,BootstrapModalModule
+        ],
     declarations:[
         ModalTestComponent
+        ,ConfirmComponent
+    ],
+    entryComponents:[
+        ConfirmComponent
     ]
 })
 export class TestModule{}
